08c973d1c45a30ee2ec7256f9237bdee272fd275
cindyemiranda <cindy.miranda0310@gmail.com>
2022-04-11 17:20:11 -0500

    Create declare-javascript-variables.js
    Basics of Javascript

---
+++ basic-javascript/declare-javascript-variables.js
@@ -0,0 +1,17 @@
+var myName;
+/*
+storing-values-with-the-assignment-operator
+*/
+// Setup
+var a;
+a=7;
+// Only change code below this line
+/*
+assigning-the-value-of-one-variable-to-another
+*/
+// Setup
+var a;
+a = 7;
+var b;
+// Only change code below this line
+b=a;
