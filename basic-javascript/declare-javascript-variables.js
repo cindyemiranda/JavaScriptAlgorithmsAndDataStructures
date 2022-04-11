0487faff8978f01e30d11d4b3aba8cd06e02f6f1
cindyemiranda <cindy.miranda0310@gmail.com>
2022-04-11 17:33:44 -0500

    Update declare-javascript-variables.js
    Initializing variables.
    Understanding unitialized variables
    Understanding case sensitivity in variables
    Explore Differences Between the var and let keywords.

--- basic-javascript/declare-javascript-variables.js
+++ basic-javascript/declare-javascript-variables.js
@@ -15,3 +15,39 @@ a = 7;
 var b;
 // Only change code below this line
 b=a;
+
+/**Initializing Variables with the Assignment Operator */
+var xux=9;
+
+/**Declare String Variables */
+var myFirstName="Cindy";
+var myLastName="Miranda";
+
+/**Understanding Uninitialized Variables */
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+a=5;
+b=10;
+c="I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+/**Understanding Case Sensitivity in Variables */
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+/**Explore Differences Between the var and let Keywords */
+/** So unlike var, when you use let, a variable with the same name can only be declared once. */
+let catName = "Oliver";
+let catSound = "Meow!";
