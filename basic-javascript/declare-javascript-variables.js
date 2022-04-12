063f2aadea8031aeb4997384169dae9e6b30b441
cindyemiranda <cindy.miranda0310@gmail.com>
2022-04-12 17:30:24 -0500

    Update declare-javascript-variables.js
    20% of the course completed

--- basic-javascript/declare-javascript-variables.js
+++ basic-javascript/declare-javascript-variables.js
@@ -51,3 +51,60 @@ titleCaseOver = 9000;
 /** So unlike var, when you use let, a variable with the same name can only be declared once. */
 let catName = "Oliver";
 let catSound = "Meow!";
+
+/** Declare a Read-Only Variable with the const Keyword */
+// const are read-only
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+/**Add Two Numbers with JavaScript */
+const sum = 10 + 10;
+
+/**Subtract One Number from Another with JavaScript */
+const difference = 45 - 33;
+
+/**Multiply Two Numbers with JavaScript */
+const product = 8 * 10;
+/**Divide One Number by Another with JavaScript */
+const myVar = 16 / 2;
+/** Increment a Number with JavaScript*/
+let myVar1 = 87;
+// Only change code below this line
+myVar1 ++;
+/**Decrement a Number with JavaScript */
+let myVar2 = 11;
+myVar2--;
+
+/**Create Decimal Numbers with JavaScript */
+const ourDecimal = 5.7;
+
+// Only change code below this line
+let myDecimal= 23.6878;
+/**Multiply Two Decimals with JavaScript */
+const product1 = 2.0 * 2.5;
+/**Divide One Decimal by Another with JavaScript */
+const quotient1 = 4.4 / 2.0; // Change this line
+/**Finding a Remainder in JavaScript */
+const remainder = 11 % 3;
+/**Compound Assignment With Augmented Addition */
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a +=12;
+b +=9;
+c +=7;
+/**Compound assignemet with augmented substraction */
+
+a -= 6;
+b -= 15;
+c -= 1;
+/**Compound Assignment With Augmented Multiplication */
+
+a *= 5;
+b *=3;
+c *=10;
