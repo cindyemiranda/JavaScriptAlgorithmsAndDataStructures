73b384401185a8f2ec95665539264911213651b6
cindyemiranda <cindy.miranda0310@gmail.com>
2022-04-18 18:21:31 -0500

    ES6 Excercises
    34% of the free course of JavaScript ES6 on freeCodeCamp.org

---
+++ ES6/es6-excersices.js
@@ -0,0 +1,97 @@
+/**Compare Scopes of the var and let Keywords */
+function checkScope() {
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+
+/**Mutate an Array Declared with const */
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+    s[0]=2;
+    s[1]=5;
+    s[2]=7;
+    console.log(s);
+  // Using s = [2, 5, 7] would be invalid
+
+  // Only change code above this line
+}
+editInPlace();
+
+/**Prevent Object Mutation */
+function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS);
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
+
+  /**Use Arrow Functions to Write Concise Anonymous Functions */
+  const magic = () =>{
+    return new Date();
+  };
+
+  /** Write Arrow Functions with Parameters*/
+  const myConcat = (arr1, arr2) => {
+    return arr1.concat(arr2);
+  };
+  
+  console.log(myConcat([1, 2], [3, 4, 5]));
+
+  /**Set Default Parameters for Your Functions */
+  // Only change code below this line
+  const increment = (number, value=1) => number + value;
+  // Only change code above this line
+
+  /**Use the Rest Parameter with Function Parameters */
+  const sum = (...args) => {
+    return args.reduce((a, b) => a + b, 0);
+  }
+
+  /**Use the Spread Operator to Evaluate Arrays In-Place */
+  const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 =[...arr1];  // Change this line
+
+console.log(arr2);
+
+/**Use Destructuring Assignment to Extract Values from Objects */
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const {today, tomorrow}=HIGH_TEMPERATURES;
+
+// Only change code above this line
+/**Use Destructuring Assignment to Assign Variables from Objects */
+const HIGH_TEMPERATURES1 = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+  
+const {today: highToday, tomorrow: highTomorrow}=HIGH_TEMPERATURES1;
+
+
+// Only change code above this line
