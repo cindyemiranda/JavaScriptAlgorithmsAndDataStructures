2646394e060bcf89758a0e72f7ad4f952215c8fe
cindyemiranda <cindy.miranda0310@gmail.com>
2022-04-26 20:00:33 -0500

    Update debugging.js
    100% of the debugging course completed.

--- debugging/debugging.js
+++ debugging/debugging.js
@@ -49,5 +49,84 @@ let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>";
 console.log(innerHtml);
 
 /**
- * 
+ * Catch Use of Assignment Operator Instead of Equality Operator
  */
+ let x = 7;
+ let y = 9;
+ let result = "to come";
+ 
+ if(x === y) {
+   result = "Equal!";
+ } else {
+   result = "Not equal!";
+ }
+ 
+ console.log(result);
+
+ /**
+  * Catch Missing Open and Closing Parenthesis After a Function Call
+  */
+function getNine() {
+    let x = 6;
+    let y = 3;
+    return x + y;
+}
+let result1 = getNine();
+console.log(result);
+
+/**
+ * Catch Arguments Passed in the Wrong Order When Calling a Function
+ */
+ function raiseToPower(b, e) {
+    return Math.pow(b, e);
+  }
+  
+  let base = 2;
+  let exp = 3;
+  let power = raiseToPower(base, exp);
+  console.log(power);
+
+/**
+ * Catch Off By One Errors When Using Indexing
+ */
+function countToFive() {
+    let firstFive = "12345";
+    let len = firstFive.length;
+    // Only change code below this line
+    for (let i = 0; i < len; i++) {
+    // Only change code above this line
+        console.log(firstFive[i]);
+    }
+}
+countToFive();
+
+/**
+ * Use Caution When Reinitializing Variables Inside a Loop
+ */
+ function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+    let row = [];
+    for (let i = 0; i < m; i++) {
+      // Adds the m-th row into newArray
+      row=[];
+      for (let j = 0; j < n; j++) {
+        // Pushes n zeroes into the current row to create the columns
+        row.push(0);
+      }
+      // Pushes the current row, which now has n zeroes in it, to the array
+      newArray.push(row);
+    }
+    return newArray;
+  }
+  
+  let matrix = zeroArray(3, 2);
+  console.log(matrix);
+/**
+ * Prevent Infinite Loops with a Valid Terminal Condition
+ */
+ function myFunc() {
+    for (let i = 1; i <= 4; i += 2) {
+      console.log("Still going!");
+    }
+  }
