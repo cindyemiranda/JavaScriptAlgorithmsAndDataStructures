846d6553fa27220fc499537b4aea2df4ce0e4313
cindyemiranda <cindy.miranda0310@gmail.com>
2022-04-28 17:47:32 -0500

    Update basic-data-structures.js
    100% of Basic Data Structures freeCodeCamp.org

--- basic-data-structures/basic-data-structures.js
+++ basic-data-structures/basic-data-structures.js
@@ -165,5 +165,189 @@ foods.strawberries=27;
 
 console.log(foods);
 
+/**
+ * Modify an Object Nested Within an Object
+ */
+ let userActivity = {
+  id: 23894201352,
+  date: 'January 1, 2017',
+  data: {
+    totalUsers: 51,
+    online: 42
+  }
+};
 
+// Only change code below this line
+userActivity.data.online=45;
+// Only change code above this line
 
+console.log(userActivity);
+
+/**
+ * Access Property Names with Bracket Notation
+ */
+ let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+function checkInventory(scannedItem) {
+  // Only change code below this line
+  return foods[scannedItem];
+  // Only change code above this line
+}
+console.log(checkInventory("apples"));
+
+/**
+ * Use the delete Keyword to Remove Object Properties
+ */
+ let foods = {
+  apples: 25,
+  oranges: 32,
+  plums: 28,
+  bananas: 13,
+  grapes: 35,
+  strawberries: 27
+};
+
+// Only change code below this line
+delete foods.oranges;
+delete foods.plums;
+delete foods.strawberries;
+// Only change code above this line
+
+console.log(foods);
+
+/**
+ * Check if an Object has a Property
+ */
+ let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(userObj) {
+  // Only change code below this line
+  if('Alan' in userObj && 'Jeff' in userObj && 'Sarah' in userObj && 'Ryan' in userObj){
+    return true;
+  }
+  else{
+    return false
+  }
+  // Only change code above this line
+}
+
+console.log(isEveryoneHere(users));
+
+/**
+ * Iterate Through the Keys of an Object with a for...in Statement
+ */
+ const users1 = {
+  Alan: {
+    online: false
+  },
+  Jeff: {
+    online: true
+  },
+  Sarah: {
+    online: false
+  }
+}
+
+function countOnline(usersObj) {
+  // Only change code below this line
+  let count=0;
+  for(let person in usersObj){
+    //console.log(usersObj.person.online);
+    console.log(usersObj[person]["online"]);
+    if(usersObj[person]["online"]==true){
+      count++;
+    }
+   
+    
+  }
+  return count;
+  // Only change code above this line
+}
+
+console.log(countOnline(users1));
+
+/**
+ * Generate an Array of All Object Keys with Object.keys()
+ */
+ let users2 = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // Only change code below this line
+  return Object.keys(obj);
+  // Only change code above this line
+}
+
+console.log(getArrayOfUsers(users2));
+
+/**
+ * Modify an Array Stored in an Object
+ */
+ let user = {
+  name: 'Kenneth',
+  age: 28,
+  data: {
+    username: 'kennethCodesAllDay',
+    joinDate: 'March 26, 2016',
+    organization: 'freeCodeCamp',
+    friends: [
+      'Sam',
+      'Kira',
+      'Tomo'
+    ],
+    location: {
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'USA'
+    }
+  }
+};
+
+function addFriend(userObj, friend) {
+  // Only change code below this line
+  userObj.data.friends.push(friend);
+  let newArray=userObj.data.friends;
+  return newArray;
+  // Only change code above this line
+}
+
+console.log(addFriend(user, 'Pete'));
