c0ba83d3d58a3d7b3e371df3c53d55f8fc2e64a7
cindyemiranda <cindy.miranda0310@gmail.com>
2022-05-01 19:08:38 -0500

    Update object-oriented-programming.js

--- object-oriented-programming/object-oriented-programming.js
+++ object-oriented-programming/object-oriented-programming.js
@@ -303,3 +303,99 @@ Animal.prototype = {
  let duck1 = new Bird();
  let beagle6 = new Dog();
 
+ /**
+  * Add Methods After Inheritance
+  */
+  function Animal() { }
+  Animal.prototype.eat = function() { console.log("nom nom nom"); };
+  
+  function Dog() { }
+  
+  // Only change code below this line
+  Dog.prototype=Object.create(Animal.prototype);
+  Dog.prototype.constructor=Dog;
+  
+  Dog.prototype.bark =function(){
+    console.log('Woof!');
+  }
+  // Only change code above this line
+  
+  let beagle7 = new Dog();
+  beagle7.bark();
+
+/**
+ * Override Inherited Methods
+ */
+ function Bird() { }
+
+ Bird.prototype.fly = function() { return "I am flying!"; };
+ 
+ function Penguin() { }
+ Penguin.prototype = Object.create(Bird.prototype);
+ Penguin.prototype.constructor = Penguin;
+ 
+ // Only change code below this line
+ Penguin.prototype.fly=function(){
+   return "Alas, this is a flightless bird.";
+ }
+ // Only change code above this line
+ let penguin = new Penguin();
+ console.log(penguin.fly());
+
+ /**
+  * Use a Mixin to Add Common Behavior Between Unrelated Objects
+  */
+  let bird = {
+    name: "Donald",
+    numLegs: 2
+  };
+  
+  let boat = {
+    name: "Warrior",
+    type: "race-boat"
+  };
+  
+  // Only change code below this line
+  
+  let glideMixin = function(obj){
+    obj.glide=function(){
+      console.log("I am gliding");
+    }
+  }
+  glideMixin(bird);
+  glideMixin(boat);
+/**
+ * Use Closure to Protect Properties Within an Object from Being Modified Externally
+ */
+ function Bird() {
+  this.weight = 15;
+  let weight =15;
+  this.getWeight= function(){
+    return weight;
+  } ;
+
+}
+
+/**
+ * Understand the Immediately Invoked Function Expression (IIFE)
+ */
+ (function(){console.log("A cozy nest is ready");})();
+
+ /**
+  * Use an IIFE to Create a Module
+  */
+  let funModule =(function(){
+    return{
+      isCuteMixin : function(obj) {
+        obj.isCute = function() {
+          return true;
+        }
+      },
+      singMixin : function (obj){
+        obj.sing= function(){
+          console.log("Singing to an awesome tune");
+        }
+      }
+    }
+  })();
+
